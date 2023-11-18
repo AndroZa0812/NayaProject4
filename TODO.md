2c8f22620d50604bd7e52cb9f9b09fce3030f5e6
Nathan T <nathan0812@gmail.com>
2023-11-18 17:29:36 +0200

    full example

--- TODO.md
+++ TODO.md
@@ -2,5 +2,5 @@
 
 1. Create a working pipeline, training and scoring [✅]
 1. Instead of removing null values add imputers [✅]
-1. Try ensembling of models []
-1. Try adding fake records for minority class []
+1. Try ensembling of models [✅]
+1. Try adding fake records for minority class [✅]
---
+++ execute_model.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import pickle
+
+def predict(X_test):
+    # load the model and the pipeline
+    with open('model.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+    with open('pipeline.pkl', 'rb') as f:
+        pipeline = pickle.load(f)
+
+    predictions = pd.DataFrame(model.predict(pipeline.transform(X_test)), columns=['LeaveOrNot'],
+                 index=X_test.index)
+    return predictions
+
+def main():
+    # read X_test
+    X_test = pd.read_csv('X_test.csv', index_col=0)
+    predictions = predict(X_test)
+    predictions.to_csv('y_test_pred.csv')
+
+
+if __name__ == '__main__':
+    main()
